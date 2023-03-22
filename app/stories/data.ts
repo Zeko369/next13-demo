1ffd4f80eefc532b760880408f802356b66d9aae
Fran Zekan <zekan.fran369@gmail.com>
2023-03-22 14:52:03 +0100

    Init stuff

---
+++ app/stories/data.ts
@@ -0,0 +1,84 @@
+export const mockStories = [
+  {
+    id: 1,
+    srcImage:
+      "https://cdn.zeplin.io/64198059ecf6302191694943/assets/3281CB72-AE36-40D6-BF38-3EF73B010DCE-optimized.png",
+    stories: [
+      { url: "https://pbs.twimg.com/media/Fh1xF6VX0AAbGKs?format=jpg&name=large", duration: 5000 },
+      {
+        url: "https://pbs.twimg.com/media/FraYz99WcAAXNQM?format=jpg&name=4096x4096",
+        duration: 5000,
+      },
+    ],
+  },
+  {
+    id: 2,
+    srcImage:
+      "https://cdn.zeplin.io/64198059ecf6302191694943/assets/3281CB72-AE36-40D6-BF38-3EF73B010DCE-optimized.png",
+    stories: [
+      {
+        url: "https://pbs.twimg.com/media/FraYz99WcAAXNQM?format=jpg&name=4096x4096",
+        duration: 5000,
+      },
+      {
+        url: "https://pbs.twimg.com/media/Fh1xF6VX0AAbGKs?format=jpg&name=large",
+        duration: 5000,
+      },
+    ],
+  },
+  {
+    id: 3,
+    srcImage: "https://seeklogo.com/images/W/wimbledon-logo-CB203042F5-seeklogo.com.png",
+    stories: [
+      { url: "https://pbs.twimg.com/media/Fh1xF6VX0AAbGKs?format=jpg&name=large", duration: 5000 },
+      {
+        url: "https://pbs.twimg.com/media/FraYz99WcAAXNQM?format=jpg&name=4096x4096",
+        duration: 5000,
+      },
+    ],
+  },
+  {
+    id: 4,
+    srcImage: "https://getlogovector.com/wp-content/uploads/2020/04/atp-tour-inc-logo-vector.png",
+    stories: [
+      { url: "https://pbs.twimg.com/media/Fh1xF6VX0AAbGKs?format=jpg&name=large", duration: 5000 },
+      {
+        url: "https://pbs.twimg.com/media/FraYz99WcAAXNQM?format=jpg&name=4096x4096",
+        duration: 5000,
+      },
+    ],
+  },
+  {
+    id: 5,
+    srcImage: "https://getlogovector.com/wp-content/uploads/2020/04/atp-tour-inc-logo-vector.png",
+    stories: [
+      { url: "https://pbs.twimg.com/media/Fh1xF6VX0AAbGKs?format=jpg&name=large", duration: 5000 },
+      {
+        url: "https://pbs.twimg.com/media/FraYz99WcAAXNQM?format=jpg&name=4096x4096",
+        duration: 5000,
+      },
+    ],
+  },
+  {
+    id: 6,
+    srcImage: "https://getlogovector.com/wp-content/uploads/2020/04/atp-tour-inc-logo-vector.png",
+    stories: [
+      { url: "https://pbs.twimg.com/media/Fh1xF6VX0AAbGKs?format=jpg&name=large", duration: 5000 },
+      {
+        url: "https://pbs.twimg.com/media/FraYz99WcAAXNQM?format=jpg&name=4096x4096",
+        duration: 5000,
+      },
+    ],
+  },
+  {
+    id: 7,
+    srcImage: "https://getlogovector.com/wp-content/uploads/2020/04/atp-tour-inc-logo-vector.png",
+    stories: [
+      { url: "https://pbs.twimg.com/media/Fh1xF6VX0AAbGKs?format=jpg&name=large", duration: 5000 },
+      {
+        url: "https://pbs.twimg.com/media/FraYz99WcAAXNQM?format=jpg&name=4096x4096",
+        duration: 5000,
+      },
+    ],
+  },
+];
